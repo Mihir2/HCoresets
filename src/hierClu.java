7ee2d1ac7b63de4b5ed0cc6423f152170c8440c1
Mihir2 <mihirchauhan9@gmail.com>
2017-04-22 16:04:11 -0400

    Comment

--- src/hierClu.java
+++ src/hierClu.java
@@ -21,6 +21,7 @@ import weka.core.Instances;
 public class hierClu {
 	
 	public static Instances readDataFile(String filename) throws IOException {
+		//Hello World
 		System.out.println("---------------Reading data file------------------");
 		BufferedReader inputReader = null;
 		try {
